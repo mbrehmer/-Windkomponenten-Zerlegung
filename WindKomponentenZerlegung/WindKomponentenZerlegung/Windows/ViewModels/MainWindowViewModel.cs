0d0fe1ab87207809770ce342d10755b4f7d6a04c
Martin Brehmer <git@mbrehmer.de>
2017-07-15 23:10:37 +0200

    [ADD] Properties for main window content
    This commit adds all needed Properties to the view model of the main
    window.

--- WindKomponentenZerlegung/WindKomponentenZerlegung/Windows/ViewModels/MainWindowViewModel.cs
+++ WindKomponentenZerlegung/WindKomponentenZerlegung/Windows/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Windows.Media;
 
 namespace WindKomponentenZerlegung
 {
@@ -12,9 +15,16 @@ namespace WindKomponentenZerlegung
 	public class MainWindowViewModel : PropertyChangedBase, IShell
 	{
 		#region private data-members
-		private string _windowTitle;
 		private readonly IEventAggregator eventAggregator;
 		private readonly IWindowManager windowManager;
+
+		private string _windowTitle;
+
+		private int _runwayHeading;
+		private int _windDirection;
+		private int _windSpeed;
+		private List<string> _windDimension;
+		private string _selectedWindDimension;
 		#endregion
 
 		#region public properties
@@ -33,6 +43,130 @@ namespace WindKomponentenZerlegung
 				NotifyOfPropertyChange(() => WindowTitle);
 			}
 		}
+
+		/// <summary>
+		/// Gets or sets the runway heading.
+		/// </summary>
+		/// <value>
+		/// The runway heading.
+		/// </value>
+		public int RunwayHeading
+		{
+			get { return _runwayHeading; }
+			set
+			{
+				_runwayHeading = value;
+				NotifyOfPropertyChange(() => RunwayHeading);
+				NotifyOfPropertyChange(() => HeadWindText);
+				NotifyOfPropertyChange(() => CrossWindText);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the wind direction.
+		/// </summary>
+		/// <value>
+		/// The wind direction.
+		/// </value>
+		public int WindDirection
+		{
+			get { return _windDirection; }
+			set
+			{
+				_windDirection = value;
+				NotifyOfPropertyChange(() => WindDirection);
+				NotifyOfPropertyChange(() => HeadWindText);
+				NotifyOfPropertyChange(() => CrossWindText);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the wind speed.
+		/// </summary>
+		/// <value>
+		/// The wind speed.
+		/// </value>
+		public int WindSpeed
+		{
+			get { return _windSpeed; }
+			set
+			{
+				_windSpeed = value;
+				NotifyOfPropertyChange(() => WindSpeed);
+				NotifyOfPropertyChange(() => HeadWindText);
+				NotifyOfPropertyChange(() => CrossWindText);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the list of possible wind dimensions.
+		/// </summary>
+		/// <value>
+		/// The list of possible wind dimensions.
+		/// </value>
+		public List<string> WindDimension
+		{
+			get { return _windDimension; }
+			set
+			{
+				_windDimension = value;
+				NotifyOfPropertyChange(() => WindDimension);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the selected wind dimension.
+		/// </summary>
+		/// <value>
+		/// The selected wind dimension.
+		/// </value>
+		public string SelectedWindDimension
+		{
+			get { return _selectedWindDimension; }
+			set
+			{
+				_selectedWindDimension = value;
+				NotifyOfPropertyChange(() => SelectedWindDimension);
+				NotifyOfPropertyChange(() => HeadWindText);
+				NotifyOfPropertyChange(() => CrossWindText);
+			}
+		}
+
+		/// <summary>
+		/// Gets the head wind text.
+		/// </summary>
+		/// <value>
+		/// The head wind text.
+		/// </value>
+		public string HeadWindText
+		{
+			get
+			{
+				string result = String.Empty;
+
+				// TODO: implementation
+
+				return result;
+			}
+		}
+
+		/// <summary>
+		/// Gets the cross wind text.
+		/// </summary>
+		/// <value>
+		/// The cross wind text.
+		/// </value>
+		public string CrossWindText
+		{
+			get
+			{
+				string result = String.Empty;
+
+				// TODO: impelmentation
+
+				return result;
+			}
+		}
 		#endregion
 
 		#region Constructors
@@ -47,6 +181,12 @@ namespace WindKomponentenZerlegung
 			this.windowManager = windowManager;
 
 			WindowTitle = "Windkomponenten-Zerlegung";
+
+			RunwayHeading = 180;
+			WindDirection = 180;
+			WindSpeed = 0;
+			WindDimension = new List<string> { "kt", "m/s", "km/h" };
+			SelectedWindDimension = WindDimension[0];
 		}
 		#endregion
 	}
